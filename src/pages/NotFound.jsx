e3f2e2c9fe6df847812e2a9f1fcb6453ce6349b1
HannaFarha <johnnyfarha1@gmail.com>
2024-01-30 17:11:58 +0100

    ready to deploy

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from '../styles/NotFound.module.css';
+import styles from '../styles/NotFound';
 
 const NotFound = () => {
   return (
